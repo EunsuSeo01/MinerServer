d3948e36f991f52ce8649db817c19bce3d861111
EunsuSeo01 <eunsu0613@naver.com>
2022-01-16 01:50:54 +0900

    Add files related to Jwt

--- src/main/java/com/umc/miner/MinerApplication.java
+++ src/main/java/com/umc/miner/MinerApplication.java
@@ -1,21 +1,14 @@
 package com.umc.miner;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MinerApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml," // application.yml 에 붙이겠다.
-            + "C:/Users/user/config.yml"; // 로컬에 있는 이 파일을
-
     public static void main(String[] args) {
 
-        //빌더패턴
-        new SpringApplicationBuilder(MinerApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(MinerApplication.class, args);
 
         // 메모리 사용량 출력
         long heapSize = Runtime.getRuntime().totalMemory();
